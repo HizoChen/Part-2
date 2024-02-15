2026da062979097540d1fc8dbd229e79d45ab7bb
HizoChen <chen166@shernet.sheridancollege.ca>
2024-02-15 11:10:25 -0500

    week6task2 done

---
+++ Assets/Week 6/Scripts/SceneLoaderMouseInput.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SceneLoaderMouseInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetMouseButtonDown(0))
+            {
+            LoadNextScene();
+              }
+        if (Input.GetMouseButtonDown(1))
+        {
+            LoadLastScene();
+        }
+
+    }
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    public void LoadLastScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex - 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
